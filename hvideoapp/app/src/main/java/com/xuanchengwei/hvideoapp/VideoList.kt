2863145d09fffbe8cc4398b3a0287668ea5018d0
xuanchengwei <xuanchengwei@coros.com>
2024-01-02 18:42:58 +0800

    Update VideoList.kt

--- hvideoapp/app/src/main/java/com/xuanchengwei/hvideoapp/VideoList.kt
+++ hvideoapp/app/src/main/java/com/xuanchengwei/hvideoapp/VideoList.kt
@@ -1,18 +1,13 @@
 package com.xuanchengwei.hvideoapp
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.*
 import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
@@ -21,22 +16,22 @@ import com.bumptech.glide.integration.compose.GlideImage
 @Composable
 fun VideoList() {
 
-    val videoList = listOf(
-        Video(
+    val videoInfoLists = listOf(
+        VideoInfo(
             imageUrl = "http://vjs.zencdn.net/v/oceans.mp4",
-            title = "First Video Title",
+            title = "First VideoInfo Title",
             views = "1M views",
             duration = "12:35"
         ),
-        Video(
+        VideoInfo(
             imageUrl = "https://via.placeholder.com/150.jpg",
-            title = "Second Video Title",
+            title = "Second VideoInfo Title",
             views = "500K views",
             duration = "08:20"
         ),
-        Video(
+        VideoInfo(
             imageUrl = "https://via.placeholder.com/150.jpg",
-            title = "3 Video Title",
+            title = "3 VideoInfo Title",
             views = "500K views",
             duration = "08:20"
         ),
@@ -44,7 +39,7 @@ fun VideoList() {
 
 
     LazyColumn {
-        items(items = videoList.chunked(2)) { rowItems ->
+        items(items = videoInfoLists.chunked(2)) { rowItems ->
             Row(Modifier.fillMaxWidth()) {
                 rowItems.forEach { video ->
                     Column(
@@ -61,13 +56,13 @@ fun VideoList() {
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
-fun VideoCard(video: Video) {
+fun VideoCard(videoInfo: VideoInfo) {
     Card(
         modifier = Modifier.padding(1.dp)
     ) {
         Column(modifier = Modifier.fillMaxWidth()) {
             GlideImage(
-                model = video.imageUrl,
+                model = videoInfo.imageUrl,
                 contentDescription = null,
                 modifier = Modifier
                     .fillMaxSize()
@@ -75,7 +70,7 @@ fun VideoCard(video: Video) {
             )
 //            Spacer(modifier = Modifier.height(0.dp))
             Text(
-                text = video.title,
+                text = videoInfo.title,
                 modifier = Modifier.padding(8.dp)
             )
             // Add other composables for additional details like views, comments, etc.
@@ -84,7 +79,7 @@ fun VideoCard(video: Video) {
 }
 
 // You'll need to define a data class to represent the video data
-data class Video(
+data class VideoInfo(
     val imageUrl: String,
     val title: String,
     val views: String,
