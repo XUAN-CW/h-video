628c81b2192da4cfea357f40986b03d699125ee5
xuanchengwei <xuanchengwei@coros.com>
2024-01-02 19:39:22 +0800

    cd

--- hvideoapp/app/src/main/java/com/xuanchengwei/hvideoapp/VideoActivity.kt
+++ hvideoapp/app/src/main/java/com/xuanchengwei/hvideoapp/VideoActivity.kt
@@ -1,9 +1,18 @@
 package com.xuanchengwei.hvideoapp
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 import com.xuanchengwei.hvideoapp.component.VideoInfo
 import com.xuanchengwei.hvideoapp.constaint.IntentExtraKey
 
@@ -13,8 +22,36 @@ class VideoActivity : ComponentActivity() {
 
         val videoInfo = intent.getParcelableExtra(IntentExtraKey.VIDEO_ACTIVITY_KEY, VideoInfo::class.java)
         setContent {
-            Text(text = videoInfo!!.imageUrl)
+
+            val context = this
+            val player = remember {
+                ExoPlayer.Builder(context).build().apply {
+                    val mediaItem = MediaItem.fromUri(videoInfo!!.imageUrl)
+                    setMediaItem(mediaItem)
+                    prepare()
+                }
+            }
+
+            DisposableEffect(Lifecycle.Event.ON_DESTROY) {
+                onDispose {
+                    player.release()
+                }
+            }
+
+            VideoPlayer(player = player)
         }
     }
+}
+
+@Composable
+fun VideoPlayer(player: ExoPlayer) {
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { context ->
+            PlayerView(context).apply {
+                this.player = player
+            }
+        }
+    )
 }
 
