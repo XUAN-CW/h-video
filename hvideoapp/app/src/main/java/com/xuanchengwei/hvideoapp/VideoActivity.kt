b4d287e62d00fcfa57c776fef150202e5d16a833
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2024-01-19 20:33:50 +0800

    Update VideoActivity.kt

--- hvideoapp/app/src/main/java/com/xuanchengwei/hvideoapp/VideoActivity.kt
+++ hvideoapp/app/src/main/java/com/xuanchengwei/hvideoapp/VideoActivity.kt
@@ -17,14 +17,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.Lifecycle
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.SimpleExoPlayer
 import androidx.media3.ui.PlayerView
 import com.xuanchengwei.hvideoapp.component.VideoInfo
 import com.xuanchengwei.hvideoapp.constaint.IntentExtraKey
