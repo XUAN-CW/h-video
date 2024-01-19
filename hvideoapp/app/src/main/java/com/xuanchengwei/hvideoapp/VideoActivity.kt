cdfaad022584954553643d0cf284760becec617d
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2024-01-19 21:36:47 +0800

    Update VideoActivity.kt

--- hvideoapp/app/src/main/java/com/xuanchengwei/hvideoapp/VideoActivity.kt
+++ hvideoapp/app/src/main/java/com/xuanchengwei/hvideoapp/VideoActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -133,6 +134,7 @@ fun HPlayer(player: ExoPlayer) {
             ) {
 
                 Button(
+                    modifier = Modifier.size(20.dp),
                     onClick = {
                         val newPosition = player.currentPosition  - 10000L
                         player.seekTo(newPosition)
